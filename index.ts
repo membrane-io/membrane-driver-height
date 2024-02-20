e05ae8aeba5c524a0280f15fdf3dde0bcd5f647f
Justin Bennett <git@just-be.dev>
2024-02-20 00:47:43 -0500

    Initial height driver

---
+++ index.ts
@@ -0,0 +1,162 @@
+/**
+ * Driver for height.app
+ *
+ * See the [Height API docs](https://height.notion.site/API-documentation-643aea5bf01742de9232e5971cb4afda) for more information.
+ */
+import { root, state, values, handles, resolvers } from "membrane";
+
+export type State = {
+  token?: string;
+};
+
+type Options = Omit<RequestInit, "body"> & { body?: object };
+type ExtraOptions = Omit<Options, "method" | "body">;
+const api = <T>(route: string, options?: Options): Promise<T> =>
+  fetch(`https://api.height.app/${route}`, {
+    ...options,
+    method: options?.method ?? "GET",
+    body: options?.body ? JSON.stringify(options.body) : undefined,
+    headers: {
+      ...options?.headers,
+      Authorization: `api-key ${state.token}`,
+    },
+  }).then((r) => r.json());
+
+api.post = (route: string, body?: object, options?: ExtraOptions) =>
+  api(route, { method: "POST", body, ...options });
+api.patch = (route: string, body?: object, options?: ExtraOptions) =>
+  api(route, { method: "PATCH", body, ...options });
+
+const pageParams = (args: { page?: number; pageSize?: number }) => {
+  return {
+    page: Math.max(args.page ?? 1, 1),
+    pageSize: Math.min(25, args.pageSize ?? 10),
+  };
+};
+const paginate = <T>(self: any, items: T[], page: number, pageSize: number) => {
+  const pageItems = items.slice((page - 1) * pageSize, page * pageSize);
+  return {
+    items: pageItems,
+    next: pageItems.length === pageSize ? self.page({ page: page + 1 }) : null,
+  };
+};
+
+export const status: resolvers.Root["status"] = () => {
+  return !state.token ? "[Add API Token](:configure)" : "Ready";
+};
+
+export const configure: resolvers.Root["configure"] = async ({ token }) => {
+  state.token = token;
+};
+
+export function workspace() {
+  return api("workspace");
+}
+
+export function me() {
+  return api("users/me");
+}
+
+export const lists = () => ({});
+export const users = () => ({});
+export const tasks = () => ({});
+
+export const TaskList: resolvers.TaskList = {
+  gref(_, { obj }) {
+    return root.lists.one({ id: obj.id });
+  },
+  async patch(args, { self }) {
+    const id = await self.id.$get();
+    const { icon, iconHue, ...otherArgs } = args;
+    const list = await api.patch(`lists/${id}`, {
+      appearance: { icon, iconHue },
+      ...otherArgs,
+    });
+    return list;
+  },
+  async duplicate(args: { name?: string } = {}, { self }) {
+    const id = await self.id.$get();
+    return await api.post(`lists/${id}/duplicate`, args);
+  },
+};
+
+export const TaskListCollection: resolvers.TaskListCollection = {
+  async one({ id }) {
+    const { list: items } = await api<{ list: values.TaskList[] }>("lists");
+    return items.find((list) => list.id === id)!;
+  },
+
+  async page(args, { self }) {
+    const { page, pageSize } = pageParams(args);
+    const { list: items } = await api<{ list: values.TaskList[] }>("lists");
+    return paginate(self, items, page, pageSize);
+  },
+};
+
+export const Task: resolvers.Task = {
+  gref(_, { obj }) {
+    return root.tasks.one({ id: obj.id });
+  },
+  parentTask(_, { obj }) {
+    return TaskCollection.one!({ id: obj.parentTaskId }, {} as any);
+  },
+  async postMessage({ type, message }, { self }) {
+    if (type !== "comment" && type !== "description") {
+      throw new Error("Invalid message type");
+    }
+    const { id: taskId } = self.$argsAt(root.tasks.one)!;
+    // FIXME: fails saying `taskId` is not a UUID???
+    return await api.post(`activities`, { type, message, taskId });
+  },
+};
+
+export const TaskCollection: resolvers.TaskCollection = {
+  one: ({ id }) => api(`tasks/${id}`),
+  async page(args, { self }) {
+    const { page, pageSize } = pageParams(args);
+    const { list } = await api<{ list: object[] }>(
+      `tasks?filters={"deleted":{"values":[false]}}`
+    );
+    return paginate(self, list, page, pageSize);
+  },
+};
+
+export const User: resolvers.User = {
+  gref(_, { obj }) {
+    return root.users.one({ id: obj.id });
+  },
+};
+
+export const UserCollection: resolvers.UserCollection = {
+  one: ({ id }) => api(`users/${id}`),
+  async page(args, { self }) {
+    const { page, pageSize } = pageParams(args);
+    const { list } = await api<{ list: unknown[] }>("users");
+
+    return paginate(self, list, page, pageSize);
+  },
+};
+
+export const Activity: resolvers.Activity = {
+  gref(_, { obj }) {
+    return root.activities.one({ id: obj.id });
+  },
+};
+
+export const ActivityCollection: resolvers.ActivityCollection = {
+  async one({ id }, { self }) {
+    const { taskId } = self.$argsAt(root.activities)!;
+    const { list } = await api<{ list: { id: string }[] }>(
+      `activities?taskId=${taskId}`
+    );
+    return list.find((activity) => activity.id === id);
+  },
+  async page(args, { self }) {
+    const { taskId } = self.$argsAt(root.activities)!;
+    const { page, pageSize } = pageParams(args);
+    const { list } = await api<{ list: unknown[] }>(
+      `activities?taskId=${taskId}`
+    );
+    return paginate(self, list, page, pageSize);
+  },
+};
